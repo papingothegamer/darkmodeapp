251579c906d37992bf75615f21eda546f90fbe5b
papingothegamer <toluwanimidaramola8@gmail.com>
2023-08-16 22:03:50 +0200

    darkmodeapp update

--- darkmodeapp/src/App.js
+++ darkmodeapp/src/App.js
@@ -8,13 +8,32 @@ function App() {
     setIsDarkMode(prevMode => !prevMode);
   };
 
+  // Function to update the indicator color based on the background color
+  const updateIndicatorColor = () => {
+    const switchContainer = document.querySelector('.switch-container');
+    const indicator = document.querySelector('.toggle-indicator');
+
+    if (switchContainer && indicator) {
+      if (isDarkMode) {
+        indicator.style.backgroundColor = '#fff'; 
+      } else {
+        indicator.style.backgroundColor = '#000'; 
+      }
+    }
+  };
+
+ 
+  React.useEffect(() => {
+    updateIndicatorColor();
+  }, [isDarkMode]);
+
   return (
     <div className="container">
-      <div className={`switch-container ${isDarkMode ? 'dark' : ''}`} onClick={toggleDarkMode}>
-        <div className="toggle-indicator"></div>
-      </div>
       <h1>Light and Dark Mode Switch</h1>
       <p>Toggle the switch to switch between light and dark mode.</p>
+      <div className={`switch-container ${isDarkMode ? 'dark' : 'light'}`} onClick={toggleDarkMode}>
+        <div className="toggle-indicator"></div>
+      </div>
       <style>
         {`
           :root {
